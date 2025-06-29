7a5ec91a0712f1368451ff2dbccc7abd292104e6
rdrahmatriski <radenrahmatriski7@gmail.com>
2025-06-29 21:16:25 +0700

    Tipedata php

---
+++ tipedata.php
@@ -0,0 +1,15 @@
+<?php
+
+$NIM = "701240181";
+$nama = "Rd Rahmat Rizky";
+$umur = 19;
+$nilai = 75.80;
+
+echo "nrp :". $NIM . "</br>";
+echo "nama : $nama</br>";
+
+print "umur :". $umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digital dibelakang koma
+printf ("nilai : %.3f</br>", $nilai);
+
+?>
